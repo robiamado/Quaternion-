ca15c6777fe2eecaa0a6743ca93d588d9ef60f31
Roberto Amadori <85162168+robiamado@users.noreply.github.com>
2021-06-28 11:48:29 +0200

    Update QuaternionPP.cpp

--- QuaternionPP.cpp
+++ QuaternionPP.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string>
-#include "Quaternione++.h"
+#include "QuaternionPP.h"
 using namespace std;
 
 ostream& operator<<(ostream& o, quaternione q)
